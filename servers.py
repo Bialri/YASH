9fa5971a476d90076dd65a369349812e74c3b86c
Bialri <kardasov96@mail.ru>
2023-10-27 17:12:28 +0300

    feat(servers): docstrings and comments

--- servers.py
+++ servers.py
@@ -18,9 +18,10 @@ class Server(ABC):
 
     @staticmethod
     def get_local_ip():
+        """Return actual ip address in local network"""
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
-            # doesn't even have to be reachable
+            # trying to connect to unreachable host
             s.connect(('192.255.255.255', 1))
             ip = s.getsockname()[0]
         except:
@@ -39,6 +40,7 @@ class Server(ABC):
 
 
 class TCPServer(Server):
+    """TCP server that handle devices wanted to connect to hub and prepare registration functions for them"""
 
     def __init__(self, loop, port, registrator: Registrator):
         self.port = port
@@ -47,10 +49,11 @@ class TCPServer(Server):
         self.registrator = registrator
 
     async def run_server(self):
+        """Start server and yield as generator clients wanted to connect"""
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind(('', self.port))
         server.listen()
-        server.settimeout(50)
+        server.settimeout(50)  # setting timeout to prevent infinite socket.recv wait with no data
         print("Tcp server started")
         while True:
             try:
@@ -58,14 +61,16 @@ class TCPServer(Server):
             except TimeoutError:
                 continue
             print('client addr: ', addr)
+            # Process client request, receive client name and prepared registration function
             name, registration_function = await self._handle_client(conn)
-            # if name is not None:
-            yield name, registration_function
+            if name is not None:
+                yield name, registration_function
             if self.stop:
                 break
 
     @staticmethod
     def _validate_request(json_string, schema):
+        """Return validated by received schema object"""
         try:
             device_specification = schema.model_validate_json(json_string)
         except ValidationError as e:
@@ -73,10 +78,12 @@ class TCPServer(Server):
         return device_specification
 
     async def _response(self, client, data):
+        """Send response to client"""
         data_encoded = data.encode()
         await self.loop.sock_sendall(client, data_encoded)
 
     async def _response_error(self, client, error_type, exception):
+        """Send error to client"""
         error = ErrorForm(status='failure',
                           type=error_type,
                           detail=str(exception))
@@ -84,19 +91,23 @@ class TCPServer(Server):
         await self._response(client, response)
 
     async def rollback(self, device_id):
+        """Rollback all data created by registrator"""
         await self.registrator.db_rollback(device_id)
         await self.registrator.emqx_acl_rollback(device_id)
         await self.registrator.emqx_user_rollback(device_id)
 
     async def _handle_client(self, client):
+        """Return clients name and client registration function"""
         input_data = (await self.loop.sock_recv(client, 1024)).decode()
         try:
             device_specification = self._validate_request(input_data, DeviceSpecification)
+        # send error and abort client registration function creation
         except RegistrationError as e:
             await self._response_error(client, 'Wrong format', e)
             return None, None
 
         async def register_client():
+            """Return True if device is registered and False if not"""
             try:
                 response = await self.registrator.register_device(device_specification)
             except ExceptionGroup:
@@ -107,16 +118,19 @@ class TCPServer(Server):
             await self._response(client, json.dumps(response))
             device_id = response['clientid']
 
+            # Trying to receive confirmation from device
             try:
                 confirm_data = (await self.loop.sock_recv(client, 1024)).decode()
                 confirm_validated = self._validate_request(confirm_data, Confirm)
                 if confirm_validated.status:
                     self.stop = True
+                # Rollback registration if device send confirmation status False
                 else:
                     await self.rollback(device_id)
                     client.close()
                     return False
 
+            # Rollback registration if device send not correct data or doesn't send any data
             except RegistrationError as e:
                 await self.rollback(device_id)
                 await self._response_error(client, 'Wrong format', e)
@@ -133,6 +147,8 @@ class TCPServer(Server):
 
 
 class BroadcastServer(Server):
+    """UDP server that handle broadcast requests from devices and
+    helps them find hub ip address in local network and port"""
 
     def __init__(self, port, tcp_port):
         self.port = port
@@ -140,15 +156,17 @@ class BroadcastServer(Server):
         self.event = multiprocessing.Event()
 
     def stop(self):
+        # Set event to stop server
         self.event.set()
 
     def run_server(self):
+        # Start server and handle requests
         self.event.clear()
         server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         server.bind(('', self.port))
-        server.settimeout(5.0)
+        server.settimeout(5.0)  # Set timeout to prevent infinite sock.recv wait with no data
         print('udp server started')
 
         while True:
@@ -165,6 +183,7 @@ class BroadcastServer(Server):
             self._handle_client(server, request)
 
     def _handle_client(self, sock, client_data):
+        """Send to client tcp server address and port"""
         recieved_data = client_data[0]
         client_addr = client_data[1]
         print(f'Recieved from: {client_addr[0]}\n data: {recieved_data}')
